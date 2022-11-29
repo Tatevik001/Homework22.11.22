370269398b2be9ae2bc2b31ff6561835c4c2de37
Tatevik Kalanchyan <tatev.kalanchyan@gamil.com>
2022-11-29 18:54:04 +0300

    Zadacha29

--- Zadacha29/Program.cs
+++ Zadacha29/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из N элементов и выводит их на экран. Вывод сделать отдельным методом.
+//5 -> [1, 2, 5, 7, 19]
+//3 -> [6, 1, 33]
+
+int[] numbers = new int[];
+Console.Write("[");
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(0, 11);
+    Console.Write(" " + Method(i) + " ");
+}
+Console.Write("]");
+int Method(int a)
+{
+    return numbers[a];
+}
