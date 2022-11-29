339145e1d43dcc981397868fc7c5ea88d712d916
Tatevik Kalanchyan <tatev.kalanchyan@gamil.com>
2022-11-29 19:16:20 +0300

    Zadacha29

--- Zadacha29/Program.cs
+++ Zadacha29/Program.cs
@@ -2,7 +2,9 @@
 //5 -> [1, 2, 5, 7, 19]
 //3 -> [6, 1, 33]
 
-int[] numbers = new int[];
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
 Console.Write("[");
 for (int i = 0; i < numbers.Length; i++)
 {
